a633e1fbbdb73af7a3cb236c45b309df448f500f
kdcontent <kdcontent18@gmail.com>
2024-07-16 16:28:04 +0900

    Make cart quantity interactive

--- js-amazon/scripts/amazon.js
+++ js-amazon/scripts/amazon.js
@@ -79,6 +79,16 @@ document.querySelectorAll('.js-add-cart')
       quantity: 1
     });
   }
+
+  let cartQuantity = 0;
+
+   cart.forEach((item) =>{
+    cartQuantity += item.quantity;
+   })
+
+    console.log(cartQuantity);
     console.log(cart);
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     })
   })
