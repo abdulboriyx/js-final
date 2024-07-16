cd5e0b485c1822ba7534b79fb39795f6fb0dd7dc
kdcontent <kdcontent18@gmail.com>
2024-07-16 16:15:42 +0900

    Most essential part for me

--- js-amazon/scripts/amazon.js
+++ js-amazon/scripts/amazon.js
@@ -47,7 +47,7 @@ products.forEach((product) =>{
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-cart" data-product-name="${product.name}">
         Add to Cart
       </button>
     </div>
@@ -55,3 +55,29 @@ products.forEach((product) =>{
   `
 });
 document.querySelector('.products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () =>{
+    const productName = button.dataset.productName;
+
+    let matchingitem;
+
+    cart.forEach((item) =>{
+      if (productName === item.productName) {
+        matchingitem = item;
+      }
+    });
+
+
+    if (matchingitem) {
+      matchingitem.quantity +=1;
+    } else {
+    cart.push({
+      productName: productName,
+      quantity: 1
+    });
+  }
+    console.log(cart);
+    })
+  })
